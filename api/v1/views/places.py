ee5892c05b6349e553feb679683f0c1dd4d05b03
AngeiraT <thaispur04@icloud.com>
2022-12-09 12:00:35 -0400

    edited put

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -50,6 +50,7 @@ def delete_place(place_id=None):
 
 @app_views.route('/cities', methods=['POST'], strict_slashes=False)
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
+
 def create_place():
     """ create a Place """
     try:
