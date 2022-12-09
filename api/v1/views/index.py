3259b2314e1683548a1a91b9fb9ac8ad4f5d781d
spcruz5 <spcruz5@gmail.com>
2022-12-08 19:47:58 -0500

    Adding city index and state

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """index.py to connect to API"""
 from api.v1.views import app_views
-from flask import jsonify
-
+from flask import Flask, Blueprint, jsonify
+from models import storage
 
 hbnbText = {
     "amenities": "Amenity",
@@ -19,6 +19,13 @@ def hbnbStatus():
     """hbnbStatus"""
     return jsonify({"status": "OK"})
 
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """Stats for task 5"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
 
 if __name__ == "__main__":
     pass
